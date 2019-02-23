923299bfe64f10fea0fcb4a15e0a850b2ae8e822
jnanda12 <jnanda63@gmail.com>
2019-02-23 13:20:24 -0700

    Added user input, clean it and create speech.csv

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import os
 
 import pandas as pd
+import keras
+from keras.preprocessing import image
+from keras import backend as K
+
+import pandas as pd
 import numpy as np
 
 import sqlalchemy
@@ -8,11 +13,48 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
 app.config['DEBUG'] = True
+
+def load_model():
+    global model
+    global graph
+    model = keras.models.load_model("mnist_trained.h5")
+    graph = K.get_session().graph
+
+load_model()
+
+def clean_user_input(files):  
+    with open(files) as file:
+        data = file.read() 
+        split_speech = data.splitlines(True)
+        final_list = []
+        for i in split_speech:
+            i = i.strip('\n')
+            if i == '':
+                pass
+            else:
+                final_list.append(i)
+        #print(final_list)
+        speech = ' '.join(final_list)
+        party = ''
+        #print(speech)
+        speech = speech.replace('.', ' ')
+        speech = speech.replace(',', ' ')
+        speech = speech.replace('“', ' ')
+        speech = speech.replace('”', ' ')
+        speech = speech.replace(':', ' ')
+        speech = speech.replace('’', ' ')
+        speech = speech.replace('$', ' ')
+        speech = speech.replace('—', ' ')
+        print(len(speech))
+
+    df = pd.DataFrame({'Speech': speech}, index=[0])
+    df.to_csv('speech.csv', sep=',', index=False)
 
 # Database Setup
 #################################################
@@ -34,11 +76,44 @@ app.config['DEBUG'] = True
 #     return df.to_json()
 
 @app.route("/")
+
 def index():
     return render_template("index.html")
 
-@app.route("/party-prediction")
+@app.route("/party-prediction", methods=['GET', 'POST'])
 def element_correlation():
+    data = {"success": False}
+    if request.method == 'POST':
+        print(request)
+
+        if request.files.get('file'):
+            # read the file
+            file = request.files['file']
+
+            # read the filename
+            filename = file.filename
+
+            # create a path to the uploads folder
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+            # Save the file to the uploads folder
+            file.save(filepath)
+            clean_user_input(filepath)
+
+            # use model to clean up 'speech.csv' i.e. cleaned = (cleaned csv)
+
+            # global graph
+            # with graph.as_default():
+
+            #     # Use the model to make a prediction
+            #     prediction_digit = model.predict_classes(cleaned)
+            #     data["prediction"] = str(prediction)
+
+            #     # indicate that the request was a success
+            #     data["success"] = True
+
+            return jsonify(data)
+
     return render_template("party-prediction.html")
 
 @app.route("/twitter")
